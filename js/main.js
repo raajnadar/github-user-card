d38309a6c5b00b5a105dc8e8e73c7f476920d5fd
Rajendran Nadar <raajnadar@gmail.com>
2018-03-04 21:23:12 +0530

    Basic ajax test

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.onreadystatechange = function () {
+	
+	function getLocation(data) {
+		if (data.location != 'null') {
+			return `lives in ${ data.location }`;
+		} else {
+			return '';
+		}
+	}
+	
+	/* wait for the contents to load */
+	if (document.readyState === 'complete') {
+		var query = document.querySelector('#retrieve');
+		var result = document.querySelector('#result');
+		
+		query.addEventListener('click', function () {
+			// Get username from input box
+			var username = document.querySelector('#username').value;
+			
+			// Ajax call
+			var request = new XMLHttpRequest();
+			request.open('GET', 'https://api.github.com/users/' + username);
+			request.onload = function () {
+				var data = JSON.parse(request.responseText);
+				result.innerHTML =`
+					<div class="row">
+						<div class="col-3">
+							<img class="img-fluid" src="${ data.avatar_url }" />
+						</div>
+						<div class="col-9">
+							<h3 class="title">${ data.name } ${ getLocation(data) }</h3>
+						</div>
+					</div>
+				`;
+			}
+			request.send();
+		});
+	}
+}
