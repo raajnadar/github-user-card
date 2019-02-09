2bd7793b16be2e3cb2bd5731997de13effedd046
raajnadar <raajnadar@gmail.com>
2019-02-09 17:24:23 +0530

    Submit on enter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.onreadystatechange = function () {
 	
+	// Location filter
 	function getLocation(data) {
 		if (data.location != null) {
 			return `lives in ${ data.location }`;
@@ -7,54 +8,59 @@ document.onreadystatechange = function () {
 			return '';
 		}
 	}
+
+	// Fetch method
+	function fetchData() {
+		// Get username from input box
+		var username = document.querySelector('#username').value;
+		
+		// Ajax call
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://api.github.com/users/' + username);
+		request.onload = function () {
+			var data = JSON.parse(request.responseText);
+			searchForm.style.display = "none"
+			result.innerHTML =`
+				<div class="row">
+					<div class="col-3">
+						<img class="img-fluid" src="${ data.avatar_url }" />
+					</div>
+					<div class="col-9">
+						<h3 class="data-title">${ data.name } ${ getLocation(data) }</h3>
+						<p class="data-text">Bio : ${ data.bio }</p>
+						<p class="data-text">Blog : ${ data.blog }</p>
+						<div style="margin-top: 16px">
+							<div class="data-badge">Repositories ${ data.public_repos }</div>
+							<div class="data-badge">Followers ${ data.followers }</div>
+							<div class="data-badge">Following ${ data.following }</div>
+						</div>
+						<div>
+							<button class="btn" onClick="window.open('${ data.html_url }');">View ${ data.name }'s profile</button>
+							<button class="btn btn-blue" id="searchAgain">Search Again</button>
+						</div>
+					</div>
+				</div>
+			`;
+			
+			var searchAgain = document.querySelector('#searchAgain');
+	
+			searchAgain.addEventListener('click', function () {
+				searchForm.style.display = "block"
+
+				result.innerHTML = ''
+			});
+		}
+		request.send();
+	}
 	
 	/* wait for the contents to load */
 	if (document.readyState === 'complete') {
-		var query = document.querySelector('#retrieve');
 		var result = document.querySelector('#result');
 		var searchForm = document.querySelector('#search-form');
 		
-		query.addEventListener('click', function () {
-			// Get username from input box
-			var username = document.querySelector('#username').value;
-			
-			// Ajax call
-			var request = new XMLHttpRequest();
-			request.open('GET', 'https://api.github.com/users/' + username);
-			request.onload = function () {
-				var data = JSON.parse(request.responseText);
-				searchForm.style.display = "none"
-				result.innerHTML =`
-					<div class="row">
-						<div class="col-3">
-							<img class="img-fluid" src="${ data.avatar_url }" />
-						</div>
-						<div class="col-9">
-							<h3 class="data-title">${ data.name } ${ getLocation(data) }</h3>
-							<p class="data-text">Bio : ${ data.bio }</p>
-							<p class="data-text">Blog : ${ data.blog }</p>
-							<div style="margin-top: 16px">
-								<div class="data-badge">Repositories ${ data.public_repos }</div>
-								<div class="data-badge">Followers ${ data.followers }</div>
-								<div class="data-badge">Following ${ data.following }</div>
-							</div>
-							<div>
-								<button class="btn" onClick="window.open('${ data.html_url }');">View ${ data.name }'s profile</button>
-								<button class="btn btn-blue" id="searchAgain">Search Again</button>
-							</div>
-						</div>
-					</div>
-				`;
-				
-				var searchAgain = document.querySelector('#searchAgain');
-		
-				searchAgain.addEventListener('click', function () {
-					searchForm.style.display = "block"
-
-					result.innerHTML = ''
-				});
-			}
-			request.send();
+		searchForm.addEventListener('submit', function (e) {
+			e.preventDefault()
+			fetchData()
 		});
 	}
 }
