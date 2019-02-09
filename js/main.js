bd46fc9c52e5e82ea74b8cf9d3d0406a2d4dda25
raajnadar <raajnadar@gmail.com>
2019-02-09 18:08:12 +0530

    Check for 404 username

--- js/main.js
+++ js/main.js
@@ -17,38 +17,47 @@ document.onreadystatechange = function () {
 		// Ajax call
 		var request = new XMLHttpRequest();
 		request.open('GET', 'https://api.github.com/users/' + username);
-		request.onload = function () {
-			var data = JSON.parse(request.responseText);
-			searchForm.style.display = "none"
-			result.innerHTML =`
-				<div class="row">
-					<div class="col-3">
-						<img class="img-fluid" src="${ data.avatar_url }" />
-					</div>
-					<div class="col-9">
-						<h3 class="data-title">${ data.name } ${ getLocation(data) }</h3>
-						<p class="data-text">Bio : ${ data.bio }</p>
-						<p class="data-text">Blog : ${ data.blog }</p>
-						<div style="margin-top: 16px">
-							<div class="data-badge">Repositories ${ data.public_repos }</div>
-							<div class="data-badge">Followers ${ data.followers }</div>
-							<div class="data-badge">Following ${ data.following }</div>
-						</div>
-						<div>
-							<button class="btn" onClick="window.open('${ data.html_url }');">View ${ data.name }'s profile</button>
-							<button class="btn btn-blue" id="searchAgain">Search Again</button>
-						</div>
-					</div>
-				</div>
-			`;
-			
-			var searchAgain = document.querySelector('#searchAgain');
-	
-			searchAgain.addEventListener('click', function () {
-				searchForm.style.display = "block"
+		request.onreadystatechange = function () {
+			if (request.readyState == 4 ) {
+				if (request.status === 200) {
+					var data = JSON.parse(request.responseText);
 
-				result.innerHTML = ''
-			});
+					if (data.message !== 'Not Found') {
+						searchForm.style.display = "none"
+						result.innerHTML =`
+							<div class="row">
+								<div class="col-3">
+									<img class="img-fluid" src="${ data.avatar_url }" />
+								</div>
+								<div class="col-9">
+									<h3 class="data-title">${ data.name } ${ getLocation(data) }</h3>
+									<p class="data-text">Bio : ${ data.bio }</p>
+									<p class="data-text">Blog : ${ data.blog }</p>
+									<div style="margin-top: 16px">
+										<div class="data-badge">Repositories ${ data.public_repos }</div>
+										<div class="data-badge">Followers ${ data.followers }</div>
+										<div class="data-badge">Following ${ data.following }</div>
+									</div>
+									<div>
+										<button class="btn" onClick="window.open('${ data.html_url }');">View ${ data.name }'s profile</button>
+										<button class="btn btn-blue" id="searchAgain">Search Again</button>
+									</div>
+								</div>
+							</div>
+						`;
+					}
+					
+					var searchAgain = document.querySelector('#searchAgain');
+			
+					searchAgain.addEventListener('click', function () {
+						searchForm.style.display = "block"
+
+						result.innerHTML = ''
+					});
+				}  else {
+					alert('No users found!')
+				}
+			}
 		}
 		request.send();
 	}
